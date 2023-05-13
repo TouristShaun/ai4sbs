01d9e39fea3f8c884e19752296a3bfa2a0c5b592
SJ <114366633+shaunjohann@users.noreply.github.com>
2023-05-12 23:04:54 -0400

    Create easypost.py in services directory

---
+++ services/easypost.py
@@ -0,0 +1,3 @@
+# This file will contain functions for interacting with the EasyPost API.
+
+# For each function, you'll need to use the EasyPost API to create, manage, and update shipments.	# To switch to a different shipping service, you'll need to replace these functions with equivalent functions using the other service's API.
