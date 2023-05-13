b0fa659c99946b78b64639e660f94134d57c1af2
SJ <114366633+shaunjohann@users.noreply.github.com>
2023-05-12 22:56:49 -0400

    Create hubspot.py in services directory

---
+++ services/hubspot.py
@@ -0,0 +1,4 @@
+# This file will contain functions for interacting with the Hubspot API.
+
+# For each function, you'll need to use the Hubspot API to create, read, update, and delete customer data.
+# To switch to a different CRM, you'll need to replace these functions with equivalent functions using the other CRM's API.
