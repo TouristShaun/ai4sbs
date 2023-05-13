8006e007bea8dbb39e9a74c98a7a1539f60af807
SJ <114366633+shaunjohann@users.noreply.github.com>
2023-05-12 23:28:04 -0400

    Add create_spreadsheet function to google_sheets.py

--- services/google_sheets.py
+++ services/google_sheets.py
@@ -1,4 +1,10 @@
-# This file will contain functions for interacting with the Google Sheets API.
+import gspreadsheets
+from google.auth.credentials import ServiceAccountCredentials
 
-# For each function, you'll need to use the Google Sheets API to create, read, update, and delete spreadsheets.
-# To switch to a different spreadsheet service, you'll need to replace these functions with equivalent functions using the other service's API.
+def create_spreadsheet(name, sheet_names):
+    creds = ServiceAccountCredentials.from_file('your-service-account.json')
+    client = gspreadsheets.Client(creds)
+    spreadsheet = client.create(spreadsheet=name, shout='true')
+    for name in sheet_names:
+        spreadsheet.add_worksheet(name=name, rows=10, cols=10)
+    return spreadsheet
