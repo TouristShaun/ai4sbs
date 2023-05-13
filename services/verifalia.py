57d6ea8913897f3a45179bf45f79fa13dcf66db6
SJ <114366633+shaunjohann@users.noreply.github.com>
2023-05-12 23:06:43 -0400

    Create verifalia.py in services directory

---
+++ services/verifalia.py
@@ -0,0 +1,3 @@
+# This file will contain functions for interacting with the Verifalia API.
+
+; For each function, you'll need to use the Verifalia API to verify email addresses.	# To switch to a different email verification service, you'll need to replace these functions with equivalent functions using the other service's API.
