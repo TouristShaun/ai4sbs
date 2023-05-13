803e403713beab9eac830dce95baa7b2bdd63795
SJ <114366633+shaunjohann@users.noreply.github.com>
2023-05-12 23:07:30 -0400

    Create postman.py in services directory

---
+++ services/postman.py
@@ -0,0 +1,4 @@
+# This file will contain functions for interacting with the Postman API.
+
+# For each function, you'll need to use the Postman API to create, read, update, and delete API requests.
+# To switch to a different API testing service, you'll need to replace these functions with equivalent functions using the other service's API.
