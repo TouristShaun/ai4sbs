b45a831fb832f76e48fe34e68e8922fa2afc2738
SJ <114366633+shaunjohann@users.noreply.github.com>
2023-05-12 23:05:53 -0400

    Create google_sheets.py in services directory

---
+++ services/google_sheets.py
@@ -0,0 +1,4 @@
+# This file will contain functions for interacting with the Google Sheets API.
+
+# For each function, you'll need to use the Google Sheets API to create, read, update, and delete spreadsheets.
+# To switch to a different spreadsheet service, you'll need to replace these functions with equivalent functions using the other service's API.
