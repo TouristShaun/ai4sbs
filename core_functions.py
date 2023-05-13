7d235ca707f30a755accd05b020c5d4e65c4156e
SJ <114366633+shaunjohann@users.noreply.github.com>
2023-05-13 01:13:17 -0400

    Create core_functions.py with generic business logic functions

---
+++ core_functions.py
@@ -0,0 +1,11 @@
+def create_db_record(table, data):
+    # Code to create a new record in the specified table with the given data
+    pass
+
+def send_email(recipient, subject, body):
+    # Code to send an email using Mailgun
+    pass
+
+def charge_credit_card(card, amount):
+    # Code to charge a credit card using Stripe
+    pass
