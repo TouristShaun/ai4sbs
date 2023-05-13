25b3820cbd42d85dcf73f25014fbbebcb9cf0f08
SJ <114366633+shaunjohann@users.noreply.github.com>
2023-05-12 22:54:22 -0400

    Create services directory and mailgun.py

---
+++ services/mailgun.py
@@ -0,0 +1,4 @@
+# This file will contain functions for interacting with the Mailgun API.
+
+# For each function, you'll need to use the Mailgun API to send emails, get status of sent emails, etc.
+# To switch to a different email service, you'll need to replace these functions with equivalent functions using the other service's API.
