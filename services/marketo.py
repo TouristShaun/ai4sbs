c9f24764b52478840b6a7918d570d36c7384f9b3
SJ <114366633+shaunjohann@users.noreply.github.com>
2023-05-12 22:57:57 -0400

    Create marketo.py in services directory

---
+++ services/marketo.py
@@ -0,0 +1,4 @@
+# This file will contain functions for interacting with the Marketo API.
+
+# For each function, you'll need to use the Marketo API to manage marketing campaigns, sending marketing emails, etc.
+# To switch to a different marketing automation service, you'll need to replace these functions with equivalent functions using the other service's API.
