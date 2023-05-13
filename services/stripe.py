acb8576da05c32d7befa264fa932367098f37f67
SJ <114366633+shaunjohann@users.noreply.github.com>
2023-05-12 23:20:22 -0400

    Add create_charge function to stripe.py

--- services/stripe.py
+++ services/stripe.py
@@ -1,4 +1,15 @@
-# This file will contain functions for interacting with the Stripe API.
+import stripe
 
-; For each function, you'll need to use the Stripe API to create charges, manage refunds, etc.
-# To switch to a different payment gateway, you'll need to replace these functions with equivalent functions using the other gateway's API.
+stripe.api_key = "your-api-key"
+
+def create_charge(amount, currency, card_number, card_expiry_month, card_expiry_year, card_cvc):
+    return stripe.Charge.create(
+        amount=amount,
+        currency=currency,
+        card={
+            "number": card_number,
+            "expiry_month": card_expiry_month,
+            "expiry_year": card_expiry_year,
+            "cvc": card_cvc
+        }
+    )
