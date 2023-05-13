fd6d0cf6c4b079b639bc81c1decf9965065e2fee
SJ <114366633+shaunjohann@users.noreply.github.com>
2023-05-12 22:55:22 -0400

    Create stripe.py in services directory

---
+++ services/stripe.py
@@ -0,0 +1,4 @@
+# This file will contain functions for interacting with the Stripe API.
+
+; For each function, you'll need to use the Stripe API to create charges, manage refunds, etc.
+# To switch to a different payment gateway, you'll need to replace these functions with equivalent functions using the other gateway's API.
