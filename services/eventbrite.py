b5e4a984cdd6412ed63fb21892862581c25e84fb
SJ <114366633+shaunjohann@users.noreply.github.com>
2023-05-12 23:03:49 -0400

    Create eventbrite.py in services directory

---
+++ services/eventbrite.py
@@ -0,0 +1,4 @@
+# This file will contain functions for interacting with the Eventbrite API.
+
+# For each function, you'll need to use the Eventbrite API to create, manage, and update events.
+# To switch to a different event management platform, you'll need to replace these functions with equivalent functions using the other platform's API.
