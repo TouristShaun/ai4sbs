d8d9245c8cf082a063ae541eaf681f5e6528e4ad
SJ <114366633+shaunjohann@users.noreply.github.com>
2023-05-12 23:12:44 -0400

    Create main.py for the core functionality of the application

---
+++ main.py
@@ -0,0 +1,10 @@
+# This file will contain the core functionality of the application.
+# It will import and use the functions we've defined in the other files.
+
+def main():
+    pass
+
+#if __name__ == '__main__':
+   import sys
+
+    sys.exit(main(sys.argv))
